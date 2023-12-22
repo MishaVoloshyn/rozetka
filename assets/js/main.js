ec4e927de881a05463783c568c1f5911702563d2
Maria Shevchenko <sevcenkomaria72@gmail.com>
2023-12-22 15:53:21 +0200

    add products in cart + show price

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 // Корзина
 
-
 let products = [
     {
         id: 1,
         full_name: 'Монитор 23.8" Acer Nitro VG240YM3bmiipx (UM.QV0EE.304) FHD IPS / 180Hz / 1 ms / 8-Bit / sRGB 99% / FreeSync Premium / Adaptive-Sync / G-Sync Сompatible / Speakers 2w',
         name: '',
+        brand: 'Acer',
         image: '1.webp',
         prev_price: 6799,
         price: 5599,
@@ -25,6 +25,7 @@ let products = [
         id: 2,
         full_name: 'Монитор 23.8" Asus TUF Gaming VG246H1A (90LM08F0-B01170) IPS FHD 100Гц / 0,5 мс (MPRT) / Extreme Low Motion Blur / 110% sRGB / AMD FreeSync / Adaptive Sync / Low Blue Light / Shadow Boost / Flicker-free',
         name: '',
+        brand: 'Asus',
         image: '2.webp',
         prev_price: 5699,
         price: 4999,
@@ -44,6 +45,7 @@ let products = [
         id: 3,
         full_name: 'Монитор 27" Samsung Odyssey AG50 S27AG502NI (LS27AG502PIXCI)',
         name: '',
+        brand: 'Samsung',
         image: '3.webp',
         prev_price: 13699,
         price: 4999,
@@ -63,6 +65,7 @@ let products = [
         id: 4,
         full_name: 'Монитор ASUS 24” VY249HGE (90LM06A5-B02370) IPS FHD 144Гц / 8-Bit / Adaptive Sync / Freesync Premium',
         name: '',
+        brand: 'ASUS',
         image: '4.webp',
         prev_price: 6499,
         price: 5999,
@@ -82,6 +85,7 @@ let products = [
         id: 5,
         full_name: 'Монитор 27" MSI Optix G27CQ4 E2 -- QHD VA Curved 170Hz / 8-Bit + FRC / sRGB 114% / Adaptive Sync / G-SYNC Compatible / Freesync Premium / Console Mode 120Hz',
         name: '',
+        brand: 'MSI',
         image: '5.webp',
         prev_price: 10999,
         price: 8999,
@@ -101,6 +105,7 @@ let products = [
         id: 6,
         full_name: 'Монитор 27" Acer Nitro VG271UM3bmiipx (UM.HV1EE.301) 2K QHD IPS / 180Hz / 1 ms (GtG) / 8-Bit / HDR10 / FreeSync Premium / G-Sync Сompatible',
         name: '',
+        brand: 'Acer',
         image: '6.webp',
         prev_price: 11999,
         price: 9599,
@@ -120,6 +125,7 @@ let products = [
         id: 7,
         full_name: 'Mонитор 23.5" Samsung Curved LS24C366 (LS24C366EAIXCI)',
         name: '',
+        brand: 'Samsung',
         image: '7.webp',
         prev_price: 5299,
         price: 4399,
@@ -139,6 +145,7 @@ let products = [
         id: 8,
         full_name: 'Монитор 27" HP Omen Hewlett-Packard Gaming Monitor 27c (35D67AA) QHD VA 1000R / 240Hz / 1 ms / 8-Bits / Local Dimming 8 зон / DisplayHDR 400 / 92% DCI-P3 / FreeSync Premium Pro / G-SYNC Compatible',
         name: '',
+        brand: 'HP',
         image: '8.webp',
         prev_price: 16399,
         price: 12599,
@@ -162,6 +169,7 @@ let products = [
         id: 10,
         full_name: 'Монитор 27" MSI Gaming Quantum Dot G274QPF QD -- Rapid IPS 1ms GtG / 2K 170Hz / 8 Bit + FRC / DCI-P3 95% / DisplayHDR 400 / G-Sync Compatible / FreeSync Premium',
         name: '',
+        brand: 'MSI',
         image: '9.webp',
         prev_price: 13999,
         price: 12399,
@@ -181,6 +189,7 @@ let products = [
         id: 10,
         full_name: 'Монитор 25" ASRock CL25FF -- FHD IPS / 100Hz / 1ms (MPRT) / 8-Bit / sRGB 99% / FreeSync / AMD Adaptive Sync / Flicker Free / Low Blue Light',
         name: '',
+        brand: 'ASRock',
         image: '10.webp',
         prev_price: 5299,
         price: 4099,
@@ -200,6 +209,7 @@ let products = [
         id: 11,
         full_name: 'Монитор 23.8" Samsung S24С330 (LS24C330GAIXCI) FullHD IPS / 100Hz / 4 мс (GtG) / 8-Bit / sRGB 100% / AMD FreeSync / Adaptive-Sync',
         name: '',
+        brand: 'Samsung',
         image: '11.webp',
         prev_price: 4999,
         price: 4399,
@@ -219,6 +229,7 @@ let products = [
         id: 12,
         full_name: 'Монитор 23.8" Asus VG249Q (90LM05E0-B03170) Adaptive-Sync / G-SYNC Compatible / FreeSync Premium',
         name: '',
+        brand: 'Asus',
         image: '12.webp',
         prev_price: 7599,
         price: 6999,
@@ -240,6 +251,7 @@ let products = [
         id: 13,
         full_name: 'Монитор 23.8" Acer Nitro Gaming VG240YEbmiix (UM.QV0EE.E09) FHD IPS / 100 Гц / 1 ms VRB / AMD FreeSync / BlueLightShield / Flickerless / Low Dimming',
         name: '',
+        brand: 'Acer',
         image: '13.webp',
         prev_price: 5299,
         price: 4599,
@@ -259,6 +271,7 @@ let products = [
         id: 14,
         full_name: 'Mонитор 31.5" Samsung Odyssey G5 LC32G55T Black (LC32G55TQWIXCI / LC32G55TQBIXCI)',
         name: '',
+        brand: 'Samsung',
         image: '14.webp',
         prev_price: 10999,
         price: 10299,
@@ -280,6 +293,7 @@ let products = [
         id: 15,
         full_name: 'Mонитор 31.5" Samsung Odyssey G5 LC32G55T Black (LC32G55TQWIXCI / LC32G55TQBIXCI)',
         name: '',
+        brand: 'Samsung',
         image: '15.webp',
         prev_price: 6999,
         price: 5499,
@@ -299,6 +313,7 @@ let products = [
         id: 16,
         full_name: 'Монитор 28" Samsung Odyssey G7 LS28BG702 (LS28BG702EIXUA) UHD 4K HDR400 / IPS 8-Bit + FRC / 144Гц / DCI-P3 90% / G-SYNC Compatible / AMD FreeSync Premium Pro / HDMI 2.1 Console Ready / Tizen Smart TV / Wi-Fi / Bluetoot',
         name: '',
+        brand: 'Samsung',
         image: '16.webp',
         prev_price: 20999,
         price: 18999,
@@ -325,126 +340,89 @@ let products = [
 
 let list = document.querySelector('.list');
 let body = document.querySelector('body');
-let total = document.querySelector('.total');
-let quantity = document.querySelector('.cart_count');
 
+let counter1 = document.querySelector('#cart_count1');
+let counter2 = document.querySelector('#cart_count2');
+
+//работа с элементами корзины
+let sum = document.querySelector('#sum');
+let sale = document.querySelector('#sale');
+let total = document.querySelector('#total');
+
+
+let listCards = [];
+let promo = ["Rozetka2024", "mm227", "ASUSF17"];
+let promoCode = document.querySelector("#promoCode");
 
 let template = document.querySelector('#template').innerHTML;
 let output = document.querySelector("#output");
+CheckCounter();
 
-// const starsElement = document.getElementById('stars');
-// const stars = starsElement.getElementsByTagName(i);
-// if(products[i].stars > 0)
-// {
-//     stars.classList.add('text-yellow');
-// }
-
+//добавление товаров на страницу
 for (let i = 0; i < products.length; i++)
 {
     products[i].name  = products[i].full_name.slice(0, 60);
-
     const productData = products[i];
     let html = Mustache.render(template, productData);
     output.insertAdjacentHTML("beforeend", html);
 }
-console.log(products[0].name);
-// openShopping.addEventListener('click', ()=>{
-//     body.classList.add('active');
-// })
-// closeShopping.addEventListener('click', ()=>{
-//     body.classList.remove('active');
-// })
-//
-// let products = [
-//     {
-//         id: 1,
-//         name: 'PRODUCT NAME 1',
-//         image: '1.PNG',
-//         price: 120000
-//     },
-//     {
-//         id: 2,
-//         name: 'PRODUCT NAME 2',
-//         image: '2.PNG',
-//         price: 120000
-//     },
-//     {
-//         id: 3,
-//         name: 'PRODUCT NAME 3',
-//         image: '3.PNG',
-//         price: 220000
-//     },
-//     {
-//         id: 4,
-//         name: 'PRODUCT NAME 4',
-//         image: '4.PNG',
-//         price: 123000
-//     },
-//     {
-//         id: 5,
-//         name: 'PRODUCT NAME 5',
-//         image: '5.PNG',
-//         price: 320000
-//     },
-//     {
-//         id: 6,
-//         name: 'PRODUCT NAME 6',
-//         image: '6.PNG',
-//         price: 120000
-//     }
-// ];
-// let listCards  = [];
-// function initApp(){
-//     products.forEach((value, key) =>{
-//         let newDiv = document.createElement('div');
-//         newDiv.classList.add('item');
-//         newDiv.innerHTML = `
-//             <img src="image/${value.image}">
-//             <div class="title">${value.name}</div>
-//             <div class="price">${value.price.toLocaleString()}</div>
-//             <button onclick="addToCard(${key})">Add To Card</button>`;
-//         list.appendChild(newDiv);
-//     })
-// }
-// initApp();
-// function addToCard(key){
-//     if(listCards[key] == null){
-//         // copy product form list to list card
-//         listCards[key] = JSON.parse(JSON.stringify(products[key]));
-//         listCards[key].quantity = 1;
-//     }
-//     reloadCard();
-// }
-// function reloadCard(){
-//     listCard.innerHTML = '';
-//     let count = 0;
-//     let totalPrice = 0;
-//     listCards.forEach((value, key)=>{
-//         totalPrice = totalPrice + value.price;
-//         count = count + value.quantity;
-//         if(value != null){
-//             let newDiv = document.createElement('li');
-//             newDiv.innerHTML = `
-//                 <div><img src="image/${value.image}"/></div>
-//                 <div>${value.name}</div>
-//                 <div>${value.price.toLocaleString()}</div>
-//                 <div>
-//                     <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
-//                     <div class="count">${value.quantity}</div>
-//                     <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
-//                 </div>`;
-//             listCard.appendChild(newDiv);
-//         }
-//     })
-//     total.innerText = totalPrice.toLocaleString();
-//     quantity.innerText = count;
-// }
-// function changeQuantity(key, quantity){
-//     if(quantity == 0){
-//         delete listCards[key];
-//     }else{
-//         listCards[key].quantity = quantity;
-//         listCards[key].price = quantity * products[key].price;
-//     }
-//     reloadCard();
-// }
+
+let addButton = document.getElementsByClassName("cartBut");
+
+const buttons = document.getElementsByTagName("button");
+const result = document.getElementById("result");
+
+function CountPrice() {
+    let sum = 0;
+    for (let i = 0; i < listCards.length; i++) {
+        sum += listCards[i].price;
+    }
+    return sum;
+}
+
+function CheckCounter()
+{
+    if(listCards.length === 0){
+        counter1.innerHTML = 0;
+        counter2.innerHTML = 0;
+        sum.innerHTML = 0;
+        sale.innerHTML = 0;
+        total.innerHTML = 0;
+    }
+    else{
+        counter1.textContent = listCards.length;
+        counter1.textContent = listCards.length;
+        sum.textContent = CountPrice();
+    }
+}
+
+
+//поиск id кликнутой кнопки
+const buttonPressed = e => {
+    let cardId = e.target.id-1;
+    addToCart(cardId);
+    result.innerHTML = `ID of <em>${e.target.innerHTML}</em> is <strong>${e.target.id}</strong>`;
+}
+
+for (let button of addButton) {
+    button.addEventListener("click", buttonPressed);
+}
+
+//функция
+let templateCard = document.querySelector('#templateCard').innerHTML;
+let listCard = document.querySelector('#listCard');
+function addToCart(id)
+{
+    const productInCart = products[id];
+    listCards.push(productInCart);
+    console.log(listCards);
+    let html = Mustache.render(templateCard, productInCart);
+    listCard.insertAdjacentHTML("beforeend", html);
+    CheckCounter();
+}
+
+
+
+
+
+
