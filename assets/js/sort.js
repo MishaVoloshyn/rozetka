8713f656f61d26d93e6e97a465b2491581164e0e
Misha Voloshyn <vol.mykhailo@gmail.com>
2023-12-25 07:22:58 +0200

    finish

--- assets/js/sort.js
+++ assets/js/sort.js
@@ -1,19 +1,20 @@
-const filterBOX = document.querySelectorAll('.box')
-console.log(filterBOX)
+window.onload = function () {
+    const filterBOX = document.querySelectorAll('.box')
+    console.log(filterBOX)
 
-document.querySelector('nav').addEventListener('click', event =>{
+    document.querySelector('nav').addEventListener('click', event => {
 
-    if( event.target.tagName !== 'LI')
-    {
-        return false;
-    }
-
-    let filterClass = event.target.dataset['f'];
-
-    filterBOX.forEach(elem => {
-        elem.classList.remove('hide');
-        if (!elem.classList.contains(filterClass)){
-            elem.classList.add('hide');
+        if (event.target.tagName !== 'LI') {
+            return false;
         }
+
+        let filterClass = event.target.dataset['f'];
+
+        filterBOX.forEach(elem => {
+            elem.classList.remove('hide');
+            if (!elem.classList.contains(filterClass)) {
+                elem.classList.add('hide');
+            }
+        })
     })
-})
+}
